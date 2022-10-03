c98f91d9da2cc960d9c04cb63791d87ccd545128
Shwe <shwe05@gmail.com>
2022-10-03 16:36:37 +0800

    Second Commit

---
+++ r_studio/R Markdown.Rmd.md
@@ -0,0 +1,10 @@
+## For loop R code for printing Int 1-10
+
+```{r}
+printint <- function() { 
+  for(x in 1:10)
+    print (x)
+}
+
+printint()
+```
--- r_studio/print_integers.R
+++ r_studio/print_integers.R
@@ -5,3 +5,17 @@ printint <- function() {
 
 
 printint()
+
+
+
+###WHILE LOOP VERSION 
+
+
+whileloop <- function() {
+  a <- 1 
+  while (a < 10) 
+    print(a)
+    a <- a + 1 
+}
+
+whileloop()
